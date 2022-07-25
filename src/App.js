812df2c3a064af8db09ec16b4e1a28fbe2863da7
Moringa <alinoorshukri8@gmail.com>
2022-07-25 12:09:58 +0300

    removed un necessary codes and added a new state for loading and events data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import Loading from './Loading';
+import Events from './Events';
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [events, setEvents] = useState([])
+
+  if (loading) {
+    return (
+      <div>
+        <Loading />
+      </div>
+    )}
   return (
-    <div className="App">
-     
+    <div>
+      <Events  />
     </div>
   );
 }
