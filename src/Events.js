754cc24697c43cbdb7302d92a3206adf7596c239
Moringa <alinoorshukri8@gmail.com>
2022-07-29 11:58:29 +0300

    mapped through the events

--- src/Events.js
+++ src/Events.js
@@ -1,7 +1,20 @@
-function Events() {
-    return ( <div>
-        <h3>Events components</h3>
-    </div> );
+import Event from "./Event";
+
+function Events({events, removeTour}) {
+    return ( 
+        <section>
+        <div className="title">
+          <h2>our Events</h2>
+          <div className="underline"></div>
+        </div>
+        <div>
+          {events.map((event) => {
+            // console.log(event);
+            return <Event key={event.id}  {...event}  removeTour={removeTour}   />;
+          })}
+        </div>
+      </section>
+    );
 }
 
 export default Events;
