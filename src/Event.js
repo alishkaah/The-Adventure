2eb358580a1efcb04c3b392ef0f05537b7054c2b
Moringa <alinoorshukri8@gmail.com>
2022-07-29 11:57:55 +0300

    made a change to a  single event componrnt

--- src/Event.js
+++ src/Event.js
@@ -1,7 +1,41 @@
-function Event() {
-    return ( <div>
-        <h3>Event component</h3>
-    </div> );
+import React, { useState } from 'react';
+
+
+function Event({ id, image, info, name, price, removeTour}) {
+    const [readMore, setReadMore] = useState(false);
+
+
+    // function HandleremoveTour(id) {
+    // //   console.log("clicked")
+    // //   fetch(`http://localhost:3000/events/${id}`, {
+    // //     method: "DELETE",
+    // //   })
+    // //     .then((r) => r.json())
+    // //     .then((data) => console.log(data));
+    // // }
+    return (
+        <article className="single-event">
+        <img src={image} alt={name} />
+        <footer>
+          <div className="event-info">
+            <h4>{name}</h4>
+            <h4 className="event-price">${price}</h4>
+          </div>
+          <p>
+            {readMore ? info : `${info.substring(0, 200)}...`}
+            <button onClick={() => setReadMore(!readMore)}>
+              {readMore ? 'show less' : '  read more'}
+            </button>
+          </p>
+          <button className="delete-btn" onClick={() => removeTour(id)}>
+            not interested
+          </button>
+        </footer>
+        
+
+        
+      </article>
+     );
 }
 
 export default Event;
